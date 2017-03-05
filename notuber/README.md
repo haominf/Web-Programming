19ddb865577a1dbd9b82dbdd82a6f58680f5e117
HAOMIN FENG <haomin.feng@tufts.edu>
2017-03-05 18:08:16 -0500

    completed assignment 2

---
+++ notuber/README.md
@@ -0,0 +1,19 @@
+#COMP 20 Web Programming Assignment 2
+
+The Black Car Service
+
+###Summary
+The required JSON API, a third-party Javascript API (Google Maps), the HTML5 geolocation API and the XMLHttpRequest object to make requests and manage the response data have been correctly implemented, which follow:
+
+* The HTML file must be named index.html in a folder named notuber in your private Git repository. You can write your JavaScript in the HTML file
+* The JavaScript navigator.geolocation object is used to retrieve the geolocation information (latitude and longitude)
+* The JavaScript XMLHttpRequest object is used to make requests (i.e., send your information) and manage the response data
+* The locations of all passengers OR vehicles are displayed, with an info window for each of which
+* All the files passed the validator
+
+###Acknowledgement
+I finished all the work by myself. But I used StackOverflow for help a lot.
+
+###Hours spent
+Approximately 7 hours.
+
---
+++ notuber/map.js
@@ -0,0 +1,109 @@
+var myLat = 0;
+var myLng = 0;
+var request = new XMLHttpRequest();
+var me = new google.maps.LatLng(myLat, myLng);
+var myOptions =  {
+	zoom: 13,
+	center: me,
+	mapTypeId: google.maps.MapTypeId.ROADMAP
+};
+var map;
+var marker;
+var infowindow = new google.maps.InfoWindow();
+
+function init()
+{
+	map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+	getMyLocation();
+}
+
+function getMyLocation()
+{
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function(position) {
+			myLat = position.coords.latitude; 	
+			myLng = position.coords.longitude;
+			render_MyLocation();
+			connect_database("s4lIFLCg", myLat, myLng);
+		});
+	}
+	else {
+		alert("Geolocation is not supported by your web browser. What a shame!");
+	}
+}
+
+function render_MyLocation()
+{
+	me = new google.maps.LatLng(myLat, myLng);
+	map.panTo(me);
+	marker = new google.maps.Marker({
+		position: me,
+		title: "I'm here!",
+		icon: "me.png"
+	});
+	marker.setMap(map);
+
+	google.maps.event.addListener(marker, 'click', function() {
+		infowindow.setContent("My username: s4lIFLCg");
+		infowindow.open(map, this);
+	});
+}
+
+function connect_database(username, mylat, mylng)
+{
+	request.open("POST", "https://defense-in-derpth.herokuapp.com/submit", true);
+	request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    
+	request.onreadystatechange = function () {
+		if (request.readyState == 4 && request.status == 200) {
+			data = request.responseText;
+			parsed = JSON.parse(data);
+			if (parsed["vehicles"]) {
+				for (i = 0; i < parsed["vehicles"].length; i++) {
+					var pic = "black_car.png";
+					render_OthersLocation(parsed["vehicles"][i], pic);
+				}
+			}
+			if (parsed["passengers"]) {
+				for (i = 0; i < parsed["passengers"].length; i++) {
+					var pic = "passenger.png";
+					render_OthersLocation(parsed["passengers"][i], pic);
+				}
+			}
+		}
+	}
+	request.send("username=" + username + "&lat=" + mylat + "&lng=" + mylng);
+}
+
+function render_OthersLocation(info, pic) 
+{
+	var others = new google.maps.LatLng(info["lat"], info["lng"]);
+	marker = new google.maps.Marker({
+		position: others,
+		icon: pic
+	});
+	marker.setMap(map);
+	var distance = calculate_distance(info["lat"], info["lng"]);
+
+	google.maps.event.addListener(marker, 'click', function() {
+		infowindow.setContent("Username: " + info["username"] + " is " + distance + " miles away!");
+		infowindow.open(map, this);
+	});
+}	
+
+function calculate_distance(lat, lng)
+{
+	Number.prototype.toRadians = function() {
+		return this * Math.PI / 180;
+	}
+	var R = 3961; // in miles
+	var φ1 = myLat.toRadians();
+	var φ2 = lat.toRadians();
+	var Δφ = (myLat-lat).toRadians();
+	var Δλ = (myLat-lat).toRadians();
+	var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) + Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ/2) * Math.sin(Δλ/2);
+	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+	var d = R * c;
+
+	return d;
+}
